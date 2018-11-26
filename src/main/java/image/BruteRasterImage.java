d61d0213accadfb6420c0f54296afbb2ccd39f0e
Armand Broutin <armand.broutin@etu.univ-amu.fr>
2018-11-26 16:42:05 +0100

    ajout des constructeurs dans BruteRasterImage

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -7,19 +7,34 @@ public class BruteRasterImage implements Image{
     private int width,height;
     private Color[][] pixels;
 
+    public BruteRasterImage(Color color,int width,int height){
+        this.width=width;
+        this.height=height;
+        this.pixels=new Color[width][height];
+        setPixelsColor(color);
+    }
+
+    public BruteRasterImage(Color[][] pixels){
+        this.width=pixels.length;
+        this.height=pixels[0].length;
+        this.pixels=pixels;
+    }
 
     public void setPixelColor(Color color,int x,int y){
         this.pixels[x][y]=color;
     }
+
     @Override
     public Color getPixelColor(int x,int y) {
         return pixels[x][y];
     }
+
     public void setPixelsColor(Color[][] pixels){
         setWidth(pixels.length);
         setHeight(pixels[0].length);
         this.pixels=pixels;
     }
+
     public void setPixelsColor(Color color){
         for (int x=0;x<this.width;x++){
             for (int y=0;y<this.height;y++){

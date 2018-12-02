f874778e158c4ddf954946388a27485d5c0beb5b
Armand Broutin <armand.broutin@etu.univ-amu.fr>
2018-12-02 23:24:26 +0100

    modifications de Display et des RasterFactory pour tester SparseRasterImage

--- src/main/java/viewer/Display.java
+++ src/main/java/viewer/Display.java
@@ -27,9 +27,11 @@ public class Display implements Initializable {
         //imageFactory = new RasterUniformImageFactory(200,200, Color.RED, RasterImageType.BRUTE);
         // affiche le drapeau de la france
         // version BruteRasterImage
-        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.BRUTE);
+        //imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.BRUTE);
         // version PaletteRasterImage
         //imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.PALETTE);
+        // version SparseRasterImage
+        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.SPARSE);
         this.image = imageFactory.makeImage();
 
         render();

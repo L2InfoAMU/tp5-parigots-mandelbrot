bf4a5f43072f1f087e447e6d0c0eae3bdc488f95
Armand Broutin <armand.broutin@etu.univ-amu.fr>
2018-12-02 22:33:09 +0100

    ajout des méthodes abstraites et des setters de la classe RasterImage

--- src/main/java/image/RasterImage.java
+++ src/main/java/image/RasterImage.java
@@ -7,12 +7,42 @@ public abstract class RasterImage<E> implements Image{
     private int width,height;
     private E[][] matrix;
 
+
+    public abstract void createRepresentation();
+    public abstract void setPixelColor(Color color, int x, int y);
     public abstract Color getPixelColor(int x, int y);
 
+    private void setPixelsColor(Color[][] pixels){
+
+        for (int x=0;x<this.width;x++){
+            for (int y=0;y<this.height;y++) {
+                setPixelColor(pixels[x][y],x,y);
+            }
+        }
+    }
+
+    private void setPixelsColor(Color color){
+
+        for (int x=0;x<this.width;x++){
+            for (int y=0;y<this.height;y++){
+                setPixelColor(color,x,y);
+            }
+        }
+    }
+    @Override
     public int getWidth(){
         return this.width;
     }
+    @Override
     public int getHeight(){
         return this.height;
+    }
+
+    protected void setWidth(int width) {
+        this.width = width;
+    }
+
+    protected void setHeight(int height) {
+        this.height = height;
     }
 }

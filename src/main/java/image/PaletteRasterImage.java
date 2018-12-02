608b40df8f5d6d15ec6eca7fb650645ae9a12420
Armand Broutin <armand.broutin@etu.univ-amu.fr>
2018-12-02 22:04:29 +0100

    ajout des arguments et des get/setPixelColor(s) de la classes PaletteRasterImage qui sont basiquement des copiés collés de la classe BruteRasterImage sauf pour le setPixelColor qui fonctionne différement a cause de la liste et de la matrice d'entier à la place de la matrice de couleurs.

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -2,8 +2,13 @@ package image;
 
 import javafx.scene.paint.Color;
 
+import java.util.List;
+
 public class PaletteRasterImage implements Image{
     private int width,height;
+    private List<Color> palette;
+    private int[][] indexesOfColor;
+
     public PaletteRasterImage(Color color, int width, int height){
 
     }
@@ -14,17 +19,28 @@ public class PaletteRasterImage implements Image{
 
     }
     public void setPixelColor(Color color, int x, int y){
-
+        if(!palette.contains(color)){
+            palette.add(color);
+        }
+        indexesOfColor[x][y]=palette.indexOf(color);
     }
     @Override
     public Color getPixelColor(int x, int y){
-        return null;
+        return palette.get(indexesOfColor[x][y]);
     }
     public void setPixelsColor(Color[][] pixels){
-
+        for (int x=0;x<this.width;x++){
+            for (int y=0;y<this.height;y++) {
+                setPixelColor(pixels[x][y],x,y);
+            }
+        }
     }
     private void setPixelsColor(Color color){
-
+        for (int x=0;x<this.width;x++){
+            for (int y=0;y<this.height;y++){
+                setPixelColor(color,x,y);
+            }
+        }
     }
     @Override
     public int getWidth() {
